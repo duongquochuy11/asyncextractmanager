9796c2c30aa09b6f666cfbab88406bafe67c4440
duongquochuy11 <mricentralpun0suy@hotmail.com>
2024-05-07 16:52:54 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-function zigzagLevelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  let zigzag = false;
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      if (zigzag) currentLevel.unshift(node.val);
-      else currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    result.push(currentLevel);
-    zigzag = !zigzag;
+    arr[j + 1] = key;
   }
-  return result;
+  return arr;
 }
